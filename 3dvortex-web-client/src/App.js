32b6a5123d30ed58b53170f554faec8fc665b0df
LautaroNavarro <navarro_lautaro@hotmail.com>
2019-11-24 23:32:22 -0300

    Starting app

---
+++ 3dvortex-web-client/src/App.js
@@ -0,0 +1,16 @@
+import React, {PureComponent} from 'react';
+import './App.css';
+
+
+
+class App extends PureComponent {
+  render () {
+  return (
+    <div>
+
+    </div>
+  );
+  }
+}
+
+export default App;
